845a11689fed76fb00b9f7ab0feffea182ca1d4c
Thiago Carretero <thiagocarretero433@gmail.com>
2023-06-21 20:45:32 -0300

    Atualizando os metodos e Criando: UpdateID e GetByID

--- galeriaBack/src/main/java/com/example/galeriaBack/controller/GaleriaController.java
+++ galeriaBack/src/main/java/com/example/galeriaBack/controller/GaleriaController.java
@@ -8,28 +8,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("galerias")
 public class GaleriaController {
 
     @Autowired
     private GaleriaRepositoryJPA repository;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping
+    @PostMapping("/galerias")
     public void saveAll(@RequestBody GaleriaRequestDTO dados) {
 
-        Galeria galeria= new Galeria(dados);
+        Galeria galeria = new Galeria(dados);
 
         repository.save(galeria);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @GetMapping
+    @GetMapping("/galerias")
     public List<GaleriaResponseDTO> getAll() {
         List<GaleriaResponseDTO> listaGaleria = repository.findAll().stream().map(GaleriaResponseDTO::new).toList();
         return listaGaleria;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/galerias/{id}")
+    public GaleriaResponseDTO getOneId(@PathVariable("id") UUID id) {
+
+        Galeria galeria = repository.findById(id).orElseThrow(() -> new RuntimeException("Não ENCONTRADO"));
+
+        GaleriaResponseDTO galeriaResponseDTO = new GaleriaResponseDTO(galeria);
+        return galeriaResponseDTO;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping("/galerias/{id}")
+    public void Update(@PathVariable("id") UUID id, @RequestBody GaleriaRequestDTO dados) {
+        Galeria galeriaOptional = repository.findById(id).orElseThrow(() -> new RuntimeException("NÃO ENCONTRADO"));
+
+        galeriaOptional.setTitulo(dados.titulo());
+        galeriaOptional.setImage(dados.image());
+        galeriaOptional.setDescricao(dados.descricao());
+
+        repository.save(galeriaOptional);
+
+    }
 }
